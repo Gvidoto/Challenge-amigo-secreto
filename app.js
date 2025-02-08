72742f9ad4d1c2b54c0f86296c596017db4f36f9
Gabriel Vidoto <vidoto_gabriel@yahoo.com>
2025-02-08 16:13:02 -0400

    Botão adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = []; 
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo) { 
+        nomes.push(amigo); 
+        let listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.innerHTML = nomes;
+        limparCampo();
+    } else {
+        alert('Por favor, insira um nome.');
+    }
+} 
+
+function limparCampo(){
+    amigo = document.getElementById('amigo');
+    amigo.value = '';
+}
+ 
+  
+        
+    
+
+
+
